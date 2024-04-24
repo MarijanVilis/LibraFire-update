85e6430482d1cb8be906583e2ca5ff3c27322791
Marijan Viliš <marijan.vilis@librafire.com>
2024-04-24 14:45:30 +0200

    Update HomePage.cy.js

--- cypress/e2e/HomePage.cy.js
+++ cypress/e2e/HomePage.cy.js
@@ -1,8 +1,14 @@
+/// <reference types="cypress" />
 
 
 describe('Homepage', () => {
-    beforeEach(() => cy.visit('https://devlf.com/lfwebsite-update/'));
 
+
+  beforeEach(() => {
+    cy.visit('https://devlf.com/lfwebsite-update/', { timeout: 10000 })}
+    
+  )
+   
   it('Check if the title is correct', () => {
     cy.title().should('eq', 'Web development and web design agency: LibraFire')
   });
@@ -17,12 +23,42 @@ describe('Homepage', () => {
     });
   });
 
-  it('Check the status of all links on the page', () => {
-    cy.get('a').each(link => {
-      cy.request(`${link.prop('href')}`).its('status').then(statusCode => {
-        expect(statusCode).to.equal(200);
-      });
-    });
+  it('Check if the contact us form has the correct text', () => {
+    cy.get('#field_qh4icy2').should('have.attr', 'placeholder', 'Name')
+    cy.get('#field_424bb').should('have.attr', 'placeholder', 'Email')
+    cy.get('#field_9jv0r12').should('have.attr', 'placeholder', 'What we need to know?')
+    cy.get('button[type="submit"]').should('have.text', 'Send Message')
+    })
+    
+ 
+ 
+  it('Check the contact form on empty submission', () => {
+    cy.get('button[type="submit"]').click()
+    cy.contains('p', 'There was a problem with your submission. Errors are marked below.').should('be.visible').should('contain', 'There was a problem with your submission. Errors are marked below.')
+    cy.get('div.frm_first div').should('be.visible').should('contain', 'Name cannot be blank.')
+    cy.get('div.your_email div').should('be.visible').should('contain', 'Email cannot be blank.')
+    cy.get('div.frm_full div').should('be.visible').should('contain', 'Message cannot be blank.')   
   });
-  
+
+it('Check successful submision of the contact form', () => {
+  cy.get('#field_qh4icy2').type('Automation test')
+  cy.get('#field_424bb').type('autotest@bla.com')
+  cy.get('#field_9jv0r12').type('This is automated test message')
+  cy.get('button[type="submit"]').click()
+  cy.contains('p', 'Your responses were successfully submitted. Thank you!').should('contain', 'Your responses were successfully submitted. Thank you!')
+  cy.get('#field_qh4icy2').should('have.value', '')
+  cy.get('#field_424bb').should('have.value', '')
+  cy.get('#field_9jv0r12').should('have.value', '')
+});
+
 })
+
+
+
+
+
+
+
+
+
+
